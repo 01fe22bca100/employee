9a3fc87c2560008c4637e3d0ab790f26af1851c7
01fe22bca100 <01fe22bca100@kletech.ac.in>
2024-04-01 21:02:01 +0530

    added details

--- student.java
+++ student.java
@@ -2,37 +2,60 @@
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.Map;
 
 class Student {
-
     private String name;
+    private LocalDate dateOfBirth;
 
-    private LocalDate dateofbirth;
+    public Student(String name, String dateOfBirth) {
+        this.name = name;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        this.dateOfBirth = LocalDate.parse(dateOfBirth, formatter);
+    }
 
-public Student(String name,String dateofbirth)
-{
-
-this.name=name;
-DateTimeFormatter formatter =DateTimeFormatter.ofPattern("yyyy-MM-dd");
-this.dateofbirth =LocalDate.parse(dateofbirth, formatter);
-}
-
-    public void displaystudentname() {
-
+    public void displayStudentName() {
         System.out.println("Student Name: " + name);
     }
 
-    public void displayage() {
-
+    public void displayAge() {
         LocalDate currentDate = LocalDate.now();
-        long age = ChronoUnit.YEARS.between(this.dateofbirth, currentDate);
-        System.out.println("Age of Student is:" + age + " Years");
+        long age = ChronoUnit.YEARS.between(this.dateOfBirth, currentDate);
+        System.out.println("Age of Student is: " + age + " Years");
+    }
+     public static void main(String[] args) {
+        Student stu = new Student("Hazra", "2003-12-04");
+        stu.displayAge();
+        stu.displayStudentName();
+
+        StudentCourses studentCourses = new StudentCourses();
+        // Add some courses and marks
+        studentCourses.addCourse("Java", 80);
+        studentCourses.addCourse("Data Structures", 85);
+        studentCourses.addCourse("Operating Systems", 70);
+        // Display the courses and marks
+        studentCourses.displayCourses();
+    }
+}
+
+
+class StudentCourses {
+    private Map<String, Integer> courses;
+
+    public StudentCourses() {
+        this.courses = new HashMap<>();
     }
 
-public static void main(String[] args) 
-{
-Student stu = new Student("Anusha", "2004-07-29");
-stu.displayage();
-stu.displaystudentname();
+    public void addCourse(String courseName, int marks) {
+        courses.put(courseName, marks);
+    }
+
+    public void displayCourses() {
+        System.out.println("Courses and Marks:");
+        for (Map.Entry<String, Integer> entry : courses.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
 }
-}
+
